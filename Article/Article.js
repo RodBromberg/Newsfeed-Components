30af34eba69101522d15b48eb3a7763ebd646189
RodBromberg <rod.bromberg@yahoo.com>
2019-02-05 23:30:37 -0500

    mvp and stretch button

--- Article/Article.js
+++ Article/Article.js
@@ -1,21 +1,37 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+    constructor(domElement) {
+        // assign this.domElement to the passed in domElement
+        this.domElement = domElement;
+        // create a reference to the ".expandButton" class. 
+        this.expandButton = domElement.querySelectorAll('.expandButton')
+            // Using your expandButton reference, update the text on your expandButton to say "expand"git
+        this.expandButton.forEach(a => a.textContent = 'expand')
+            // Set a click handler on the expandButton reference, calling the expandArticle method.
+        this.removeButton = domElement.querySelectorAll('.removeButton');
+        this.removeButton.forEach((item) => item.textContent = 'Click to Remove');
+        this.removeButton.forEach((item) => item.addEventListener('click', () => this.removeArticle()));
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
 
-  }
+        this.expandButton.forEach((btn) => {
+            btn.addEventListener('click', () => {
+                this.expandArticle()
+            })
+        })
+
+
+    }
+
+    expandArticle() {
+        // Using our reference to the domElement, toggle a class to expand or hide the article.
+        this.domElement.classList.toggle('article-open')
+    }
+
+    removeArticle() {
+        this.domElement.classList.toggle('article-hide')
+    }
+
 }
 
 /* START HERE: 
@@ -26,4 +42,4 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article').forEach(a => new Article(a))
